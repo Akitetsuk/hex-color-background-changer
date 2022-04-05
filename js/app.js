d2d7a7f3b72cf39e9d618c5f25f8d88dc0842929
Akitetsuk <m.feijoo.1995@gmail.com>
2022-04-05 12:10:40 +0200

    VUE-hex-color-background-changer

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 // Implementar solución aquí
 
+Vue.createApp({
+    data() {
+        return {
+            bgColor: '#FFFFFF'
+        }
+    },
+    methods: {
+        updateRandomColor() {          
+            this.bgColor = this.generateRandomColor();;
+        },
+        generateRandomColor() {            
+            const red = Math.floor(Math.random() * 255);
+            const green = Math.floor(Math.random() * 255);
+            const blue = Math.floor(Math.random() * 255); 
+            return (`rgb(${red}, ${green}, ${blue})`);       
+    }
+}
+}).mount('#app')
+
+
